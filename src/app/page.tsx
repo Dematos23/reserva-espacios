378f835614e062f9e96f38ad3f853560307b3ad5
Diego Matos <dematos23@gmail.com>
2024-03-08 17:57:00 -0500

    Vercel sigue fregando

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import ReservaModal from "./components/ReservaModal";
+// import ReservaModal from "./components/ReservaModal";
 
 export default function Home() {
   return (
