3bc00b3e0c62ab0c8592c8616ac15d451712bb9b
Diego Matos <dematos23@gmail.com>
2024-03-11 17:35:15 -0500

    cambiada foto gita a png

--- src/app/Components/ReservaForm.js
+++ src/app/Components/ReservaForm.js
@@ -57,13 +57,15 @@ export default function ReservaForm() {
             <div className="flex">
               <Image
                 className="inline-block h-30 w-10 rounded-full ring-2 ring-blue m-2"
-                src="/gita.jpg"
+                src="/gita.png"
+                alt="Gita"
                 width={25}
                 height={25}
               />
               <Image
                 className="inline-block h-30 w-10 rounded-full ring-2 ring-blue m-2"
                 src="/nayat.png"
+                alt="Nayat"
                 width={25}
                 height={25}
               />
@@ -149,93 +151,6 @@ export default function ReservaForm() {
             <option>Sillas en fila</option>
           </select>
         </label>
-
-        {/* Dropdown de Flowbite */}
-
-        {/* <button
-          id="dropdownCheckboxButton"
-          data-dropdown-toggle="dropdownDefaultCheckbox"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          type="button"
-        >
-          Dropdown checkbox{" "}
-          <svg
-            className="w-2.5 h-2.5 ms-3"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 10 6"
-          >
-            <path
-              stroke="currentColor"
-              l-linecap="round"
-              l-linejoin="round"
-              strokeWidth="2"
-              d="m1 1 4 4 4-4"
-            />
-          </svg>
-        </button> */}
-
-        {/* Dropdown menu */}
-        {/* <div
-          id="dropdownDefaultCheckbox"
-          className="z-10 hidden w-48 bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
-        >
-          <ul
-            className="p-3 space-y-3 text-sm text-gray-700 dark:text-gray-200"
-            aria-labelledby="dropdownCheckboxButton"
-          >
-            <li>
-              <div className="flex items-center">
-                <input
-                  id="checkbox-item-1"
-                  type="checkbox"
-                  value=""
-                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                />
-                <label
-                  to="checkbox-item-1"
-                  className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                >
-                  Default checkbox
-                </label>
-              </div>
-            </li>
-            <li>
-              <div className="flex items-center">
-                <input
-                  // checked
-                  id="checkbox-item-2"
-                  type="checkbox"
-                  value=""
-                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                />
-                <label
-                  to="checkbox-item-2"
-                  className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                >
-                  Checked state
-                </label>
-              </div>
-            </li>
-            <li>
-              <div className="flex items-center">
-                <input
-                  id="checkbox-item-3"
-                  type="checkbox"
-                  value=""
-                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                />
-                <label
-                  to="checkbox-item-3"
-                  className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                >
-                  Default checkbox
-                </label>
-              </div>
-            </li>
-          </ul>
-        </div> */}
       </form>
     </div>
   );
