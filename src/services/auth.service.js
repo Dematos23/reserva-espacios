bd5170745cac30bdaefa79e3c144287da1a49ad3
Diego Matos <dematos23@gmail.com>
2024-09-17 17:29:08 -0500

    variable de entorno aceptable para Next

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001",
-  // baseURL: "https://jyotir-backend.vercel.app",
+  baseURL: process.env.NEXT_PUBLIC_URL,
   headers: {
     "content-type": "application/json",
   },
