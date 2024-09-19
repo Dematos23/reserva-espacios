21fbf888d25e13b63715650d4c14b1e2b8cd2e13
Diego Matos <dematos23@gmail.com>
2024-09-19 18:21:46 -0500

    retirado reservations types para Vercel

--- src/types/reservations.types.ts
+++ src/types/reservations.types.ts
@@ -7,8 +7,8 @@ export interface Reservation {
     endTime: Date;
     implementos?: string | null;
     observation?: string | null;
-    office?: Office | null; // Suponiendo que 'Office' es otro modelo en Prisma
-    state: ReservationState; // 'ReservationState' es un enum o tipo definido en Prisma
-    clients: Client[]; // Suponiendo que tienes un tipo 'Client' ya definido
-    users: User[];     // Suponiendo que tienes un tipo 'User' ya definido
+    // office?: Office | null; // Suponiendo que 'Office' es otro modelo en Prisma
+    // state: ReservationState; // 'ReservationState' es un enum o tipo definido en Prisma
+    // clients: Client[]; // Suponiendo que tienes un tipo 'Client' ya definido
+    // users: User[];     // Suponiendo que tienes un tipo 'User' ya definido
   }
